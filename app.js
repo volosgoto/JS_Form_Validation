2c1ad51dbca107e46543002e78c5c4556e94200f
volosgoto <volosgoto@yahoo.com>
2018-08-29 11:25:27 +0300

    add validateName()

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ document.getElementById('name').addEventListener('blur', validateName);
 document.getElementById('zip').addEventListener('blur', validateZip);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+    let name = document.getElementById('name');
+    let re = /^[a-zA-Z]{2,10}$/i;
+
+    if (!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
+}
+
+function validateZip() {}
+
+function validateEmail() {}
+
+function validatePhone() {}
