6dd59ed1ca60e36e1baa20b6d820717f483d523d
volosgoto <volosgoto@yahoo.com>
2018-08-29 11:06:16 +0300

    add eventListeners

---
+++ app.js
@@ -0,0 +1,5 @@
+// Add event listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
